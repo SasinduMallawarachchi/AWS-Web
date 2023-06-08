d3c7e6b26891fb3d8a21812935816169755a65b1
SasinduMallawarachchi <it20200206@my.sliit.lk>
2023-06-08 20:20:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function saveWebPage() {
+    // Create an S3 instance
+    const s3 = new AWS.S3();
+  
+    // Set up the parameters for the PutObject operation
+    const params = {
+      Bucket: 'your-bucket-name',
+      Key: 'webpage.html',
+      Body: document.documentElement.outerHTML,
+      ContentType: 'text/html'
+    };
+  
+    // Call the PutObject operation to save the web page to S3
+    s3.putObject(params, function(err, data) {
+      if (err) {
+        console.error('Error saving web page:', err);
+      } else {
+        console.log('Web page saved to S3!');
+      }
+    });
+  }
+  
